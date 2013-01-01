7b00702846c13b5d2eee47d065b100a64f21f272
David Wee <rook2pawn@gmail.com>
2013-01-01 05:28:30 -0800

    working on legend mouse interaction

--- examples/simple2/example2-bundle.js
+++ examples/simple2/example2-bundle.js
@@ -677,6 +677,15 @@ var legend = function(el) {
     this.legend = el; 
     this.legend.width=300;this.legend.height=200;
     this.legendctx = el.getContext('2d');
+    var jq_el = $(el);
+    jq_el.css('cursor','pointer');
+    var jq_offset_left = jq_el.offset().left;
+    var jq_offset_top = jq_el.offset().top;
+    jq_el.mousemove(function(ev) {
+        var offx = ev.pageX - jq_offset_left;
+        var offy = ev.pageY - jq_offset_top;
+        el.getContext('2d').strokeRect(offx,offy,30,10);  
+    });
 };
 var chart = function() {
     this.buffer = document.createElement('canvas');
