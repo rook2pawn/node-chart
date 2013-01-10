4abfd245d5fd3951f3ba6ae74ef179b2b0f89673
David Wee <rook2pawn@gmail.com>
2013-01-10 12:25:23 -0800

    updated interaction test

--- test/interaction.js
+++ test/interaction.js
@@ -1,16 +1,17 @@
-var lib = require('../lib/interaction');
+var Interaction = require('../lib/interaction');
+var i = new Interaction();
 var test = require('tap').test;
 test('getNeighbors',function(t) {
     t.plan(5);
     var list = [{x:2,y:3},{x:3,y:9},{x:6,y:15}];
-    var neighbors = lib.getNeighbors(4,list);
+    var neighbors = i.getNeighbors(4,list);
     t.deepEquals(neighbors,{left:{x:3,y:9},right:{x:6,y:15}});
-    neighbors = lib.getNeighbors(1,list);
+    neighbors = i.getNeighbors(1,list);
     t.deepEquals(neighbors,{left:undefined,right:{x:2,y:3}});
-    neighbors = lib.getNeighbors(2.5,list);
+    neighbors = i.getNeighbors(2.5,list);
     t.deepEquals(neighbors,{left:{x:2,y:3},right:{x:3,y:9}});
-    neighbors = lib.getNeighbors(3,list);
+    neighbors = i.getNeighbors(3,list);
     t.deepEquals(neighbors,{left:{x:3,y:9},right:{x:6,y:15}});
-    neighbors = lib.getNeighbors(7,list);
+    neighbors = i.getNeighbors(7,list);
     t.deepEquals(neighbors,{left:{x:6,y:15},right:undefined});
 });
